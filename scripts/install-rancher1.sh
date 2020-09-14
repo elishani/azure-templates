6cb4ef20975ef5c995d7c91521a3db10458684c4
Eli Shani <elishani50@gmail.com>
2020-09-14 21:18:52 +0300

    add rancher

---
+++ scripts/install-rancher1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+apt update
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt -y install \
+    docker-compose \
+    curl \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common \
+    docker-ce \
+    wget \
+    docker-compose
+sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
+
+mkdir -p /var/www/rancher
+cat > docker-compose.yml <<EOF
+version: '2'
+services:
+  rancher:
+    image: rancher/rancher:latest
+	  restart: unless-stopped
+    ports:
+      - "80:80"
+      - "443:443"
+    volumes:
+      - ./rancher:/var/lib/rancher
+      - ./auditlog:/var/log/auditlog
+EOF
+
+docker-compose -f docker-compose.yml up -d
