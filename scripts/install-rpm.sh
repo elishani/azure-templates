f56247fe4f3bdfb41da7062955f33007b927f91e
Eli Shani <elishani50@gmail.com>
2020-09-14 13:39:44 +0300

    add rpm installation

--- scripts/install-rpm.sh
+++ scripts/install-rpm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 apt update
 
-apt -y install docker.io
-systemctl start docker
-systemctl enable docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt -y install \
+    docker-compose \
+    curl \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common \
+    docker-ce \
+    wget \
+    docker-compose
+sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
+
