b13d9ff38eeae9c9fd90b38fbb8f0ecf0bac4b2e
Eli Shani <elishani50@gmail.com>
2020-09-15 08:25:14 +0300

    changes to install rancher

--- scripts/install-rancher1.sh
+++ scripts/install-rancher1.sh
@@ -14,13 +14,13 @@ apt -y install \
     docker-compose
 sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
 
+my_ip=$(ip adde )
 mkdir -p /var/www/rancher
 cat > docker-compose.yml <<EOF
 version: '2'
 services:
   rancher:
     image: rancher/rancher:latest
-	  restart: unless-stopped
     ports:
       - "80:80"
       - "443:443"
