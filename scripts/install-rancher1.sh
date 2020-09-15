4e0b4cbd6a96572e3034a2b7a19042434077b8d9
Eli Shani <elishani50@gmail.com>
2020-09-15 11:30:27 +0300

    test

--- scripts/install-rancher1.sh
+++ scripts/install-rancher1.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-#myIp=`ip addr  show eth0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1`
-#preIp=`echo $myIp | cut -d'.' -f1-3`
-#postIp=`echo $myIp | cut -d'.' -f4`
-#lastUp=`expr $postIp + 1`
-#lastDown=`exper $postIp - 1`
-#upIp="$preIp.$lastUp"
-#downIp="$preIp.$lastDown"
-
-#i=5
-#while true; do
-#	ping -c1 $upIp
-#	if [ $? = 0  ]; then
-#		station2=$upIp
-#		break
-#	fi
-#	ping -c1 $downIp
-#	if [ $? = 0  ]; then
-#                station2=$downIp
-#                break
-#	fi
-#	i=`expr $i - 1`
-#	[ $i = 0 ] && break
-#done
-#if [ -z $station2 ]; then
-#        echo "No ping to other machine"
-#else
-#        echo "$station2 rancher2" >> /etc/hosts
-#fi
-#
-
 apt update
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -62,3 +32,33 @@ services:
 EOF
 
 docker-compose -f docker-compose.yml up -d
+
+#myIp=`ip addr  show eth0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1`
+#preIp=`echo $myIp | cut -d'.' -f1-3`
+#postIp=`echo $myIp | cut -d'.' -f4`
+#lastUp=`expr $postIp + 1`
+#lastDown=`exper $postIp - 1`
+#upIp="$preIp.$lastUp"
+#downIp="$preIp.$lastDown"
+
+#i=5
+#while true; do
+#	ping -c1 $upIp
+#	if [ $? = 0  ]; then
+#		station2=$upIp
+#		break
+#	fi
+#	ping -c1 $downIp
+#	if [ $? = 0  ]; then
+#                station2=$downIp
+#                break
+#	fi
+#	i=`expr $i - 1`
+#	[ $i = 0 ] && break
+#done
+#if [ -z $station2 ]; then
+#        echo "No ping to other machine"
+#else
+#        echo "$station2 rancher2" >> /etc/hosts
+#fi
+#
