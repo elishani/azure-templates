693a27410815915e5db58c925b6423b71464f1df
Eli Shani <elishani50@gmail.com>
2020-09-17 11:00:44 +0300

    install docker

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -2,28 +2,14 @@
 
 apt update
 
-myIp=`ip addr  show eth0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1`
-preIp=`echo $myIp | cut -d'.' -f1-3`
-postIp=`echo $myIp | cut -d'.' -f4`
-lastUp=`expr $postIp + 1`
-lastDown=`expr $postIp - 1`
-upIp="$preIp.$lastUp"
-downIp="$preIp.$lastDown"
-i=5
-while true; do
-	ping -c1 $upIp
-	if [ $? = 0  ]; then
-		station2=$upIp
-		break
-	fi
-	ping -c1 $downIp
-	if [ $? = 0  ]; then
-                station2=$downIp
-                break
-	fi
-	i=`expr $i - 1`
-	[ $i = 0 ] && break
-done
-if [ -z $station2 ]; then
-        echo "No ping to other machine"
-fi
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt -y install \
+    docker-compose \
+    curl \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common \
+    docker-ce \
+    wget
+sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
