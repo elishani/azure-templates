9fb64b169c23f8aa94f260d7b38487e47fc492e0
Eli Shani <elishani50@gmail.com>
2020-09-19 01:13:26 +0300

    inst8

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -80,5 +80,5 @@ apt install -y sshpass
 su -c "sshpass -oStrictHostKeyChecking=no -p $temp_passwd ssh-copy-id $vm2" - $temp_user
 su -c "scp /tmp/join_to_kubernstes.sh $temp_user@$vm2" - $temp_user
 su -c "ssh $temp_user@$vm2 \"bash join_to_kubernstes.sh\"" - $temp_user
-su -c "ssh $temp_user@$vm2 \"userdel $temp_user\"" - $temp_user
+#su -c "ssh $temp_user@$vm2 \"userdel $temp_user\"" - $temp_user
 #userdel $temp_user
