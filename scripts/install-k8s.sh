877594833c858243220c336171ca63a6333a3183
Eli Shani <elishani50@gmail.com>
2020-09-18 11:55:05 +0300

    install k8s

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-
 apt update
+apt install -y docker.io
+systemctl enable docker
+apt update 
+apt install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt update
+apt install -y kubelet kubeadm kubectl
+#apt-mark hold kubelet kubeadm kubectl
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt -y install \
-    docker-compose \
-    curl \
-    apt-transport-https \
-    ca-certificates \
-    software-properties-common \
-    docker-ce \
-    wget
-sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
+# Master
+masterIp=`ip addr  show eth0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1`
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 | tee /tmp/join.temp
+egrep 'kubeadm join|--discovery-token-ca-cert-hash' /tmp/join.temp > $HOME/join_to_kubernstes.sh
+user=$(grep ':x:1000:1000:' /etc/passwd | awk -F: '{print $1}')
+user_id=1000
+group_id=1000
+chown $user_id:$group_id /etc/kubernetes/admin.conf
+# run as user  
+su -c "mkdir -p $HOME/.kube" - $user
+su -c "cp -i /etc/kubernetes/admin.conf $HOME/.kube/config" - $user
+su -c "kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml" - $user
