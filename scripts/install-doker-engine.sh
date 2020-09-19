d96f650092865d93ac7ed868d8a88f56f8300103
Eli Shani <elishani50@gmail.com>
2020-09-19 17:04:23 +0300

    install docker

---
+++ scripts/install-doker-engine.sh
@@ -0,0 +1,13 @@
+apt update
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt -y install \
+    docker-compose \
+    curl \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common \
+    docker-ce \
+    wget
+sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
