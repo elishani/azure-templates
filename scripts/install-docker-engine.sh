23a107f16a27ca2533531075df0d65f0c3f11e1b
Eli Shani <elishani50@gmail.com>
2020-09-20 09:52:33 +0300

    inst 8

--- scripts/install-docker-engine.sh
+++ scripts/install-docker-engine.sh
@@ -1,18 +1,17 @@
+#!/bin/bash
 apt update
-
-apt -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properyies-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt -y install \
+    docker-compose \
+    curl \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common \
+    docker-ce \
+    wget \
+    docker-compose
+sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
 
-apt fingerprint 0EBFCD88 
-
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-apt update
-
-apt install -y docker-ce docker-ce-cli containerd.io
+mkdir -p /var/www/rancher
