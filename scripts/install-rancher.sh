38fb8e36e3f22e94d95a70dd2d3f3cd60a294f94
Eli Shani <elishani50@gmail.com>
2020-09-20 15:43:59 +0300

    inst 8

---
+++ scripts/install-rancher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update
+
+# Install doker engine
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
+
+# Create ssh-keygen
+cat /dev/zero | ssh-keygen -q -N ""
