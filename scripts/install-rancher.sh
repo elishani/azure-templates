04221c6c506b5cf7782f9ef6f14d09c5650f6f56
Eli Shani <elishani50@gmail.com>
2020-09-22 09:02:06 +0300

    inst 8

--- scripts/install-rancher.sh
+++ scripts/install-rancher.sh
@@ -13,5 +13,5 @@ user=$(grep ':x:1000:1000:' /etc/passwd | awk -F: '{print $1}')
 usermod -aG docker $user
 su -c 'mkdir $HOME/.ssh' - $user
 su -c 'chmod 700 $HOME/.ssh ' - $user
-su -c 'touch $HOME/.ssh/authorized_keys' - $user
+su -c 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2BMyRL1pvYi4JmAvsgimRQWouTehJw+dyw+LT4fGF9NQ4nVYreXDPhrjlIn7SF3RW9ynGT3unyoAj+ul0MGJYbFGGI8yPv/6f3mjego7BZQCf6OOVBQf8LDWy78KEKG7Yz5uwmrAaqszC+2C91bzG8MMEBmTm/XtUPNpwzCksaK0S7ZXQmaFThEAWGkV7vMmahBklOVUlFP0tUpxdP3SObBFFUNDgh37kP6SxrXpaLXUluGWtNoZvWsYzPkEqyZES8gJAmVg0ocqQfDnaxwiar8PQH/9prr5xDcjeFYmhTTO5hDHg5Zc9d359ZuYK4gAQ2VByongEFi1oxoJifUsJU1fFe56cd6YYn0G+8b49k+D1R5YK/n4JvoxciYOqTPGDmbDHBYKCwSlDNWh9uID/6PLrUE6NP6N/5YRJe5wawMZido+cmxRQQNjq7B5+YgQe5kSD49V1ruHWo8i18QCPDSjYFXdBTvwaQ/2AQSuBerGWkvVCtr/OJI47jj1x+50= eli@eli-Latitude-5590" >$HOME/.ssh/authorized_keys' - $user
 
