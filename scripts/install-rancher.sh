58d59a2fe66b5fb5d39543a92beb85181926d55f
Eli Shani <elishani50@gmail.com>
2020-09-26 17:06:15 +0300

    inst

--- scripts/install-rancher.sh
+++ scripts/install-rancher.sh
@@ -20,25 +20,8 @@ su -c 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2BMyRL1pvYi4JmAvsgimRQWouTeh
 
 [ -z $(hostname  | grep 'vm1$') ] && exit
 
-resource_group_name=$1
+numberOfVMs=$1
+echo "'$numberOfVMs'"
+vNetSubnet=$2
+echo "'$vNetSubnet'"
 export file_name=cluster.yml
-
-az network public-ip list --resource-group $resource_group_name | grep '"ipAddress":' |  awk -F'"' '{print $4}' > iplist.txt
-
-sed -e "1d" iplist.txt > vm.txt
-
-echo "nodes:" > $file_name
-i=1
-for public in `cat vm.txt` ; do
-vm=rancher$i
-private=`ssh -o "StrictHostKeyChecking no" vm@$public /sbin/ip addr | grep 'inet 10.0' | awk '{print $2}' | cut -d'/' -f1`
-host_name=`ssh vm@$public hostname`
-cat >> $file_name <<EOF
-  - address: $public
-    internal_address: $private
-    user: vm
-    role: [controlplane, worker, etcd]
-    hostname_override: $host_name
-EOF
-i=$((++i))
-done
