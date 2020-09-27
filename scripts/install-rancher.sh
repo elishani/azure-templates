9557beb1e4d75abf429bf5f12c4d786225b6acbf
Eli Shani <elishani50@gmail.com>
2020-09-27 09:36:03 +0300

    inst8

--- scripts/install-rancher.sh
+++ scripts/install-rancher.sh
@@ -13,23 +13,28 @@ user=$(grep 'x:1000:1000:' /etc/passwd | awk -F: '{print $1}')
 usermod -aG docker $user
 su -c 'mkdir $HOME/.ssh' - $user
 su -c 'chmod 700 $HOME/.ssh' - $user
-su -c 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2BMyRL1pvYi4JmAvsgimRQWouTehJw+dyw+LT4fGF9NQ4nVYreXDPhrjlIn7SF3RW9ynGT3unyoAj+ul0MGJYbFGGI8yPv/6f3mjego7BZQCf6OOVBQf8LDWy78KEKG7Yz5uwmrAaqszC+2C91bzG8MMEBmTm/XtUPNpwzCksaK0S7ZXQmaFThEAWGkV7vMmahBklOVUlFP0tUpxdP3SObBFFUNDgh37kP6SxrXpaLXUluGWtNoZvWsYzPkEqyZES8gJAmVg0ocqQfDnaxwiar8PQH/9prr5xDcjeFYmhTTO5hDHg5Zc9d359ZuYK4gAQ2VByongEFi1oxoJifUsJU1fFe56cd6YYn0G+8b49k+D1R5YK/n4JvoxciYOqTPGDmbDHBYKCwSlDNWh9uID/6PLrUE6NP6N/5YRJe5wawMZido+cmxRQQNjq7B5+YgQe5kSD49V1ruHWo8i18QCPDSjYFXdBTvwaQ/2AQSuBerGWkvVCtr/OJI47jj1x+50= eli@eli-Latitude-5590" >$HOME/.ssh/authorized_keys' - $user
+su -c 'echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2BMyRL1pvYi4JmAvsgimRQWouTehJw+dyw+LT4fGF9NQ4nVYreXDPhrjlIn7SF3RW9ynGT3unyoAj+ul0MGJYbFGGI8yPv/6f3mjego7BZQCf6OOVBQf8LDWy78KEKG7Yz5uwmrAaqszC+2C91bzG8MMEBmTm/XtUPNpwzCksaK0S7ZXQmaFThEAWGkV7vMmahBklOVUlFP0tUpxdP3SObBFFUNDgh37kP6SxrXpaLXUluGWtNoZvWsYzPkEqyZES8gJAmVg0ocqQfDnaxwiar8PQH/9prr5xDcjeFYmhTTO5hDHg5Zc9d359ZuYK4gAQ2VByongEFi1oxoJifUsJU1fFe56cd6YYn0G+8b49k+D1R5YK/n4JvoxciYOqTPGDmbDHBYKCwSlDNWh9uID/6PLrUE6NP6N/5YRJe5wawMZido+cmxRQQNjq7B5+YgQe5kSD49V1ruHWo8i18QCPDSjYFXdBTvwaQ/2AQSuBerGWkvVCtr/OJI47jj1x+50=" >$HOME/.ssh/authorized_keys' - $user
 
 # Run on one machine
 
 [ -z $(hostname  | grep 'vm1$') ] && exit
 
 count=$1
-echo "Number of VM: '$count'"
-publicIp1=$2
-publicIp2=$3
-publicIp3=$5
-ip_array=($publicIp1 $publicIp2 $publicIp3)
-echo ${ip_array[*]}
+ips=()
+ips[0]=20.54.106.29
+[ ! -z "$3" ]&& ips[1]=20.54.107.16
+[ ! -z "$4" ]&& ips[2]=4
 
-private1=`ssh -o "StrictHostKeyChecking no" vm@$publicIp1 /sbin/ip addr | grep 'inet 10.0' | awk '{print $2}' | cut -d'/' -f1`
-private2=`ssh -o "StrictHostKeyChecking no" vm@$publicIp2 /sbin/ip addr | grep 'inet 10.0' | awk '{print $2}' | cut -d'/' -f1`
-private3=`ssh -o "StrictHostKeyChecking no" vm@$publicIp3 /sbin/ip addr | grep 'inet 10.0' | awk '{print $2}' | cut -d'/' -f1`
+echo "${ips[@]}"
+
+for i in "${ips[@]}"
+do
+        echo $i
+ssh -o "StrictHostKeyChecking no" vm@$i /sbin/ip addr | grep 'inet 10.0' | awk '{print $2}' | cut -d'/' -f1
+done
+
+exit
+
 #
 #myIp=`ip addr  show eth0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1`
 #ip_pre=`echo $myIp | cut -d'.' -f1-3`
