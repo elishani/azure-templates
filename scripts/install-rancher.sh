9588cf17e34b60ac5153dffef2ce8e382cb68da6
Eli Shani <elishani50@gmail.com>
2020-09-29 12:51:40 +0300

    inst 8

--- scripts/install-rancher.sh
+++ scripts/install-rancher.sh
@@ -109,7 +109,7 @@ chown -R $owner $home/.ssh
 chmod -R 600 $home/.ssh/*
 
 cd $home
-sleep 120
+sleep 60
 rke up
 mkdir .kube
 chown $owner .kube
@@ -125,7 +125,7 @@ kubectl create namespace cert-manager
 helm3 repo add jetstack https://charts.jetstack.io
 helm3 repo update
 helm3 install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.15.0
-sleep 30
+sleep 15
 kubectl get pods --namespace cert-manager
 helm3 install rancher rancher-latest/rancher --namespace cattle-system --set hostname=$ip_loadbalancer
 
