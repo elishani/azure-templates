fc613f7258c0e7b9d9482378603ea2f8ed1111db
Eli Shani <elishani50@gmail.com>
2020-10-21 20:04:48 +0300

    inst

--- scripts/install-okd.sh
+++ scripts/install-okd.sh
@@ -11,7 +11,7 @@ echo "Ip='$ip'"
 user_home=/home/$user
 
 sleep 60
-yum -y update
+#yum -y update
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y  docker-ce docker-ce-cli containerd.io
