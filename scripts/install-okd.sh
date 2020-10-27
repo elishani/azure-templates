77db82228cd580d954722c1f6866697b2fc9fba4
Eli Shani <elishani50@gmail.com>
2020-10-27 13:26:14 +0200

    inst

--- scripts/install-okd.sh
+++ scripts/install-okd.sh
@@ -44,6 +44,10 @@ home=/home/$user
 owner_group=`grep "^$user" /etc/passwd | cut -d':' -f4`
 docker_group=`grep docker /etc/group | cut -d':' -f 3`
 sed -i "s/:$owner_group:$owner_group:/:$owner_group:$docker_group:/" /etc/passwd
+sed -i "s/^$user://" /etc/group
+sed -i "s/:$owner_group:$owner_group:/:$owner_group:$docker_group:/" /etc/passwd
+usermod -aG docker $user
+
 
 cd $home
 wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
